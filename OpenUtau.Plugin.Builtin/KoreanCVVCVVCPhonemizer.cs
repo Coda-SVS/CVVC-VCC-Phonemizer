1bd905f190ed2a870e07ca23c74486d58c9a49ba
crlotwhite <crlotwhite@gmail.com>
2024-07-10 23:07:21 +0900

    Add korean cvvc-vvc phonemizer class

---
+++ OpenUtau.Plugin.Builtin/KoreanCVVCVVCPhonemizer.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using OpenUtau.Api;
+using OpenUtau.Core.Ustx;
+using OpenUtau.Core;
+
+namespace OpenUtau.Plugin.Builtin {
+    [Phonemizer("Korean CVVC-VVC Phonemizer", "KO CVVC-VVC", "NoelKM", language: "KO")]
+
+    public class KoreanCVVCVVCPhonemizer: Phonemizer {
+        public KoreanCVVCVVCPhonemizer() {
+
+        }
+
+        public override Result Process(Note[] notes, Note? prev, Note? next, Note? prevNeighbour, Note? nextNeighbour, Note[] prevs) {
+            throw new NotImplementedException();
+        }
+
+        public override void SetSinger(USinger singer) {
+            throw new NotImplementedException();
+        }
+    }
+}
