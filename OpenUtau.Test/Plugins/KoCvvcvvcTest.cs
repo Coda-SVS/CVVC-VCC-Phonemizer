71371401600ef1a2e7075167290c953fe96030f9
crlotwhite <crlotwhite@gmail.com>
2024-07-12 01:15:01 +0900

    Add ko_cvvcvvc test code

---
+++ OpenUtau.Test/Plugins/KoCvvcvvcTest.cs
@@ -0,0 +1,25 @@
+﻿using OpenUtau.Api;
+using OpenUtau.Plugin.Builtin;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace OpenUtau.Plugins {
+    public class KoCvvcvvcTest : PhonemizerTestBase {
+        public KoCvvcvvcTest(ITestOutputHelper output) : base(output) { }
+
+        protected override Phonemizer CreatePhonemizer() {
+            return new KoreanCVVCVVCPhonemizer();
+        }
+
+        [Theory]
+        [InlineData("ko_cvvcvvc",
+            new string[] { "안", "녕"},
+            new string[] { "C4", "C4"},
+            new string[] { "", "" },
+            new string[] {"a n", "n yeo ng"})]
+        public void PhonemizeTest(string singerName, string[] lyrics, string[] tones, string[] colors, string[] aliases) {
+            RunPhonemizeTest(singerName, lyrics, RepeatString(lyrics.Length, ""), tones, colors, aliases);
+        }
+    }
+}
+
