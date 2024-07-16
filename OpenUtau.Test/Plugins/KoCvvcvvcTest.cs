b0ea7be41ed6521bddfe01644ba6d814849add52
crlotwhite <crlotwhite@gmail.com>
2024-07-17 03:03:26 +0900

    Update test routine

--- OpenUtau.Test/Plugins/KoCvvcvvcTest.cs
+++ OpenUtau.Test/Plugins/KoCvvcvvcTest.cs
@@ -16,7 +16,7 @@ namespace OpenUtau.Plugins {
             new string[] { "안", "녕"},
             new string[] { "C4", "C4"},
             new string[] { "", "" },
-            new string[] {"a n", "n yeo ng"})]
+            new string[] {"안", "녕"})]
         public void PhonemizeTest(string singerName, string[] lyrics, string[] tones, string[] colors, string[] aliases) {
             RunPhonemizeTest(singerName, lyrics, RepeatString(lyrics.Length, ""), tones, colors, aliases);
         }
